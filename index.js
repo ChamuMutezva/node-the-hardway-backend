9fda7688fae7bcdbf1d3971c947099dbcc513880
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-11-13 06:27:09 +0200

    moved PORT to be global

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ app.use(express.static("dist"))
 
 // connect to database
 console.log(process.env.MONGO_URI)
+const PORT = process.env.PORT || 3001
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        app.use(notesRouter)
-        const PORT = process.env.PORT || 3001
+        app.use(notesRouter)       
         app.listen(PORT)
         console.log("Connected to the database")
         console.log(`Server running on port ${PORT}`)
